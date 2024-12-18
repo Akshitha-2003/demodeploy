5d18761d662818ee8dbf42fb85ab7123bf089ba9
Akshitha-2003 <kundhateakshitha@gmail.com>
2024-12-19 00:09:53 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base :"/demodeploy",
   plugins: [react()],
+  build: {
+    outDir: 'build', // Change output directory to 'build'
+  },
 })
